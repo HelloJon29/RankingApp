bacac0cf0c2dfaae54db7b438fd01f9e9fa0d77f
HelloJon29 <jonkim1996@gmail.com>
2023-09-05 20:18:33 -0500

    Created a Movie Image Component as an array using the imported images from images/movies

--- ReactApp/ClientApp/src/components/MovieImages.js
+++ ReactApp/ClientApp/src/components/MovieImages.js
@@ -1,1 +1,25 @@
-﻿
+﻿import Godfather from '../images/movies/Godfather.png';
+import Highlander from '../images/movies/Highlander.png';
+import Highlander2 from '../images/movies/Highlander2.png';
+import LastOfTheMohicans from '../images/movies/LastOfTheMohicans.png';
+import PoliceAcademy6 from '../images/movies/PoliceAcademy6.png';
+import RearWindow from '../images/movies/RearWindow.png';
+import RoadHouse from '../images/movies/RoadHouse.png';
+import Shawshank from '../images/movies/Shawshank.png';
+import StarTrack4 from '../images/movies/StarTreck4.png';
+import Superman4 from '../images/movies/Superman4.png';
+
+const MovieImageArr = [
+    { id: 1, image: Godfather },
+    { id: 1, image: Highlander },
+    { id: 1, image: Highlander2 },
+    { id: 1, image: LastOfTheMohicans },
+    { id: 1, image: PoliceAcademy6 },
+    { id: 1, image: RearWindow },
+    { id: 1, image: RoadHouse },
+    { id: 1, image: Shawshank },
+    { id: 1, image: StarTrack4 },
+    { id: 1, image: Superman4 },
+]
+
+export default MovieImageArr
