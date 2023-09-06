d36dff365bc437237304a694608fb9cd8c1926c8
HelloJon29 <jonkim1996@gmail.com>
2023-09-05 19:14:01 -0500

    Added generic ItemModel class, and setup basic Controller class for the item

---
+++ ReactApp/Models/ItemModel.cs
@@ -0,0 +1,13 @@
+﻿namespace RankingApp.Models
+{
+    public class ItemModel // this will be a generic class to be used by multiple types of items
+    {
+        public int Id { get; set; } // Note how this is done, Properties will be Pascal case
+        public String Title { get; set; } // Note how these are getter and setters, not just an attributes, but unlike java these aren't exactly methods
+        public int ImageId { get; set; }
+        public int Ranking {  get; set; }
+        public int ItemType { get; set; } // This will be used as the relational mapping to check what type of item we are dealing with
+
+
+    }
+}
